7e3ab7d2f78e6ce1292d0754561b0ad7c5e870e6
sid1511996 <siddartha1511996@gmail.com>
2016-02-13 23:07:22 +0530

    first commit

---
+++ app/contacts/contacts.js
@@ -0,0 +1,135 @@
+'use strict';
+
+angular.module('myContacts.contacts', ['ngRoute','firebase'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/contacts', {
+    templateUrl: 'contacts/contacts.html',
+    controller: 'contactsCtrl'
+  });
+}])
+
+.controller('contactsCtrl', ['$scope','$firebaseArray', function($scope,$firebaseArray) {
+	var ref = new Firebase('https://mycontacts2016.firebaseio.com/MYCONTACTS');
+	$scope.contacts = $firebaseArray(ref);
+	//console.log($scope.contacts);
+	$scope.showAddForm = function(){
+		$scope.kukka = true;
+	}
+	$scope.showEditForm = function(contact){
+		$scope.editFormShow = true;
+
+		$scope.id = contact.$id;
+		$scope.name = contact.name;
+		$scope.email = contact.email;
+		$scope.company = contact.company;
+		$scope.mobile_phone = contact.phones[0].mobile;
+		$scope.home_phone = contact.phones[0].home;
+		$scope.work_phone = contact.phones[0].work;
+		$scope.street_address = contact.address[0].street_address;
+		$scope.city = contact.address[0].city;
+		$scope.state = contact.address[0].state;
+		$scope.zipcode = contact.address[0].zipcode;
+	}
+	$scope.hide = function(){
+		$scope.kukka = false;
+		$scope.contactShow = false;
+	}
+	$scope.formSubmit = function(){
+		console.log("uploading contacts...");
+
+		if($scope.name){ var name = $scope.name; } else { var name = null; }
+		if($scope.email){ var email = $scope.email; } else { var email = null; }
+		if($scope.company){ var company = $scope.company; } else { var company = null; }
+		if($scope.mobile_phone){ var mobile_phone = $scope.mobile_phone; } else { var mobile_phone = null; }
+		if($scope.home_phone){ var home_phone = $scope.home_phone; } else { var home_phone = null; }
+		if($scope.work_phone){ var work_phone = $scope.work_phone; } else { var work_phone = null; }
+		if($scope.street_address){ var street_address = $scope.street_address; } else { var street_address = null; }
+		if($scope.city){ var city = $scope.city; } else { var city = null; }
+		if($scope.state){ var state = $scope.state; } else { var state = null; }
+		if($scope.zipcode){ var zipcode = $scope.zipcode; } else { var zipcode = null; }
+
+		$scope.contacts.$add({
+			name:name,
+			email:email,
+			company:company,
+			phones:[
+				{
+					mobile:mobile_phone,
+					home:home_phone,
+					work:work_phone
+				}
+			],
+			address:[
+				{
+					street_address:street_address,
+					city:city,
+					state:state,
+					zipcode:zipcode
+				}
+			]
+		}).then(function(ref){
+			var id = ref.key();
+			console.log('added contact with ID:'+id);
+			clearFiels();
+			$scope.kukka = false;
+			$scope.msg = "contact added";
+		});
+	}
+	
+	$scope.editFormSubmit = function(){
+		
+		console.log('updating....');
+
+		var id = $scope.id;
+		var record = $scope.contacts.$getRecord(id);
+
+		record.name = $scope.name;
+		record.email = $scope.email;
+		record.company = $scope.company;
+		record.phones[0].mobile = 	$scope.mobile_phone;
+		record.phones[0].home = $scope.home_phone;
+		record.phones[0].work = $scope.work_phone;
+		record.address[0].street_address = $scope.street_address;
+		record.address[0].city = $scope.city;
+		record.address[0].state = $scope.state;
+		record.address[0].zipcode = $scope.zipcode;
+		//save
+		$scope.contacts.$save(record).then(function(ref){
+			console.log(ref.key);
+			});
+		clearFiels();
+		$scope.editFormShow = false;
+		$scope.msg = "contact Updated";
+	}
+
+	$scope.showContact = function(contact){
+		console.log("getting contact...");
+		$scope.name = contact.name;
+		$scope.email = contact.email;
+		$scope.company = contact.company;
+		$scope.mobile_phone = contact.phones[0].mobile;
+		$scope.home_phone = contact.phones[0].home;
+		$scope.work_phone = contact.phones[0].work;
+		$scope.street_address = contact.address[0].street_address;
+		$scope.city = contact.address[0].city;
+		$scope.state = contact.address[0].state;
+		$scope.zipcode = contact.address[0].zipcode;
+		$scope.contactShow = true;
+	}
+	function clearFiels(){
+		$scope.name = '';
+		$scope.email = '';
+		$scope.company = '';
+		$scope.mobile_phone = '';
+		$scope.home_phone = '';
+		$scope.work_phone = '';
+		$scope.street_address = '';
+		$scope.city = '';
+		$scope.state = '';
+		$scope.zipcode = '';
+	}
+	$scope.removeContact = function(contact){
+		$scope.contacts.$remove(contact);
+	};
+}]);
